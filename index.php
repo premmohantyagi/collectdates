d929718aed08b85e515e1fc61cc08ad397359520
PM Tyagi <premmohantyagi@gmail.com>
2019-03-16 03:01:19 +0530

    Update dates Pattern

--- index.php
+++ index.php
@@ -9,7 +9,23 @@ function standard_date_format($str) {
     printf("By Format: %s <br>", Carbon::parse(str_replace('-', '/', '03-15-2013')) ); 
 
       
-    $matchPatternDate = '/((0?[1-9]|1[012]|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?))(-|\/|\s)(\d{1,2}(|,)?|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)(|,)?)(-|\/|\s)\d{2,4}(\s)?([01]\d|[0-9])?(:)?([0-5]\d)?(:[0-5]\d)?(\s)?(am|pm)?)/';
+    $matchPatternDate = '/(((0?[1-9]|[1-2][0-9]|3[01])|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?))(-|\/|\s|  )(\d{1,2}(|,| ,|  ,)?|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)(|,|  | ,)?)(-|\/|\s)\d{2,4}(\s)?([01]\d|[0-9])?(:)?([0-5]\d)?(:[0-5]\d)?(\s)?(am|pm)?)/';
+
+    // $matchPatternDate = '/(\d{1,2}(-|\/)\d{1,2}(-|\/)\d{2,4})|([a-z]+ [0-9]{1,2}, \d[0-9]{1,3})/';
+
+    // $matchPatternDate = '/((0?[1-9]|1[012])(-|\/)\d{1,2}(-|\/)\d{2,4}\b([1-9]|[12][0-9]|3[01])\b)|([a-z]+ [0-9]{1,2}, \d[0-9]{1,3})|[0-9]{2}\-[0-9]{2}\-[0-9]{4}/';
+
+    //  $matchPatternDate = '/((0?[1-9]|1[012])(-|\/)\d{1,2}(-|\/)\d{2,4})|([A-Z,a-z]+ [0-9]{1,2}, \d[0-9]{1,3})|([0-9]{2}\-[0-9]{2}\-[0-9]{4})/';
+
+    //  $matchPatternDate = '/(0?[1-9]|1[012]|2[0-9]|3[01])(-|\/)\d{1,2}(-|\/)\d{2,4}(?:(?:\s(0?[1-9]|[2][0-3]|[0][1-9])\:([0-5]\d)(?::([0-5]\d))?)?)\s?(AM|PM)?/'; 
+
+    // $matchPatternDate = '/((0?[1-9]|1[012])(-|\/)\d{1,2}(-|\/)\d{2,4})|([A-Z,a-z]+ [0-9]{1,2}, \d[0-9]{1,3})|([0-9]{2}\-[0-9]{2}\-[0-9]{4})(?:(?:\s(0?[1-9]|[2][0-3]|[0][1-9])\:([0-5]\d)(?::([0-5]\d))?)?)\s?(AM|PM)?/'; 
+
+    // $matchPatternDate = '/((0?[1-9]|1[012]|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?))(-|\/|\s)(\d{1,2}(|,)?|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)(|,)?)(-|\/|\s)\d{2,4}(\s)?([01]\d|[0-9])?(:)?([0-5]\d)?(:[0-5]\d)?(\s)?(am|pm)?)/'; 
+
+    // $matchPatternDate = '/((0?[1-9]|1[012]|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?))(-|\/|\s|  )(\d{1,2}(|,| ,|  ,)?|(jan(uary)?|feb(ruary)?|mar(ch)?|apr(il)?|may|jun(e)?|jul(y)?|aug(ust)?|sep(tember)?|oct(ober)?|nov(ember)?|dec(ember)?)(|,|  | ,)?)(-|\/|\s)\d{2,4}(\s)?([01]\d|[0-9])?(:)?([0-5]\d)?(:[0-5]\d)?(\s)?(am|pm)?)/';
+
+    // $matchPatternDate = '/(\d{1,2}(-|\/)\d{1,2}(-|\/)\d{2,4})/';
 
     $matches = '';    
     
